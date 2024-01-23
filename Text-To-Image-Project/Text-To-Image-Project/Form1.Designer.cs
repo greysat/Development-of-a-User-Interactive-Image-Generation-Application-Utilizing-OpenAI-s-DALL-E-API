62e1d0d4d00c2549c35d565ef713f3e0e61d3b95
Hind Hnouda <109519215+greysat@users.noreply.github.com>
2024-01-23 17:52:15 +0800

    Add files via upload

---
+++ Text-To-Image-Project/Text-To-Image-Project/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace DALLE_WinForms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button GenerateImageButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button DownloadImageButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.GenerateImageButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DownloadImageButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(776, 20);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // GenerateImageButton
+            // 
+            this.GenerateImageButton.Location = new System.Drawing.Point(12, 38);
+            this.GenerateImageButton.Name = "GenerateImageButton";
+            this.GenerateImageButton.Size = new System.Drawing.Size(150, 23);
+            this.GenerateImageButton.TabIndex = 1;
+            this.GenerateImageButton.Text = "Generate Image";
+            this.GenerateImageButton.UseVisualStyleBackColor = true;
+            this.GenerateImageButton.Click += new System.EventHandler(this.GenerateImageButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 371);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DownloadImageButton
+            // 
+            this.DownloadImageButton.Location = new System.Drawing.Point(168, 38);
+            this.DownloadImageButton.Name = "DownloadImageButton";
+            this.DownloadImageButton.Size = new System.Drawing.Size(150, 23);
+            this.DownloadImageButton.TabIndex = 3;
+            this.DownloadImageButton.Text = "Download Image";
+            this.DownloadImageButton.UseVisualStyleBackColor = true;
+            this.DownloadImageButton.Click += new System.EventHandler(this.DownloadImageButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DownloadImageButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.GenerateImageButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "DALL-E Image Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Text-To-Image-Project/Text-To-Image-Project/Form1.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DALLE_WinForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private string apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        private const string BaseUrl = "https://api.openai.com/v1/images/generations";
+        private Image generatedImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private async void GenerateImageButton_Click(object sender, EventArgs e)
+        {
+            var prompt = InputTextBox.Text;
+            try
+            {
+                var base64Image = await GenerateImage(prompt);
+
+                // Convert Base64 String to byte[]
+                byte[] imageBytes = Convert.FromBase64String(base64Image);
+
+                // Convert byte[] to Image
+                using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+                {
+                    generatedImage = Image.FromStream(ms, true);
+                    pictureBox1.Image = generatedImage;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private async Task<string> GenerateImage(string prompt)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, BaseUrl);
+
+            // Set the authorization header with your API key
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apiKey);
+
+            // Set the content of the request
+            var content = new
+            {
+                model = "dall-e-3",
+                prompt,
+                n = 1,
+                size = "1024x1024",
+                response_format = "b64_json"
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(content);
+            request.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            // Send the request and get the response
+            var response = await _httpClient.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorContent = await response.Content.ReadAsStringAsync();
+                throw new Exception($"API request failed with status code {response.StatusCode}, {errorContent}");
+            }
+
+            // Read the content of the response
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            // Extract the image data from the response
+            var responseObject = JsonConvert.DeserializeObject<dynamic>(responseContent);
+
+            // Check if the 'response_format' was 'b64_json'
+            if (content.response_format == "b64_json")
+            {
+                // The image data is in the "b64_json" property of the first item in the "data" array
+                string base64Image = responseObject.data[0]?.b64_json;
+
+                if (string.IsNullOrEmpty(base64Image))
+                {
+                    throw new Exception("API response does not contain image data");
+                }
+
+                // Print out the revised prompt
+                string revisedPrompt = responseObject.data[0]?.revised_prompt;
+                Console.WriteLine($"Revised prompt: {revisedPrompt}");
+
+                return base64Image;
+            }
+            else // 'response_format' was 'url' or not specified
+            {
+                // The URL of the image is in the "url" property of the first item in the "data" array
+                string imageUrl = responseObject.data[0]?.url;
+
+                if (string.IsNullOrEmpty(imageUrl))
+                {
+                    throw new Exception("API response does not contain image URL");
+                }
+
+                // Download the image from the URL
+                var imageResponse = await _httpClient.GetAsync(imageUrl);
+                var imageBytes = await imageResponse.Content.ReadAsByteArrayAsync();
+
+                return Convert.ToBase64String(imageBytes);
+            }
+        }
+
+        private void DownloadImageButton_Click(object sender, EventArgs e)
+        {
+            if (generatedImage != null)
+            {
+                saveFileDialog1.Filter = "JPEG Image|*.jpg";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    generatedImage.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please generate an image first.");
+            }
+        }
+    }
+}
